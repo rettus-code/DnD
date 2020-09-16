8e0f2724bd6b4005ff973f88441fcea4b1d66ba4
Rettus code <michael.rettusii@gmail.com>
2020-09-16 15:16:28 -0700

    Silvertone addition

--- src/components/pages/charachters/Silvertone.js
+++ src/components/pages/charachters/Silvertone.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function Silvertone() {
     return (
-        <div>
-            
+        <div className="row">
+            <div className="col-sm-12 col-lg-4">
+            <div className="Silvertone"></div>
+            </div>
+            <div className="col-sm-0 col-lg-1"></div>
+            <div className="col-sm-12 col-lg-5">
+            <div className="card">
+                <div className="card-header">Silvertone</div>
+                <ul class="list-group list-group-flush">
+                    <li className="list-group-item">Combat role: Support/Control <br/> Group Role: Jack of all trades and interactions with high born NPCs</li>
+                    <li className="list-group-item">If Mike is drawing your attention it is too be certain it's away from something iportant. Distracting you 
+                    while his friend cuts your purse and all the while looking for a better setup. It's not Mike you should be concerned with, but rather what 
+                    he doesn't want you to know that should have you worried.</li>
+                    <li className="list-group-item"><strong>Official Report:</strong><br/>A late comer to the new group he quickly made his presence known.
+                    Through inspirational songs or tales and a knack for having something for any situation Silvertone the group as a whole has become more 
+                    dangerous. While his support of others makes them more deadly, alone he does not seem prticularly dangerous. His father finally got back 
+                    to us and promised not to interfere, that his son was on his own. Still it is best not to over do it where a Barron's son is concerned.</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }
