5e37618f4a4236d33b1a13f4252e44521ef84bf7
Rettus code <michael.rettusii@gmail.com>
2020-09-16 14:25:13 -0700

    charachter page looks good

--- src/components/pages/charachters/Apok.js
+++ src/components/pages/charachters/Apok.js
@@ -4,17 +4,21 @@ import '../../../App.css';
 export default function Apok() {
     return (
         <div className="row">
-            <div className="col col-lg-4">
+            <div className="col-sm-12 col-lg-4">
             <div className="Apok"></div>
             </div>
-            <div className="col col-lg-1"></div>
-            <div className="col col-lg-5">
+            <div className="col-sm-0 col-lg-1"></div>
+            <div className="col-sm-12 col-lg-5">
             <div className="card">
                 <div className="card-header">Apok</div>
                 <ul class="list-group list-group-flush">
-                    <li className="list-group-item">stuff</li>
-                    <li className="list-group-item">more stuff</li>
-                    <li className="list-group-item">final stuff</li>
+                    <li className="list-group-item">Combat role: Tank/Damage <br/> Group Role: Muscle and connections with warforge in Sharn</li>
+                    <li className="list-group-item">While always ready for anything Apok is not quick to start a fight. Steady and vigilante as 
+                    the rock he was born of you will be hard pressed to catch him unaware. He prefers to watch than draw attention, though what 
+                    he is watching for can be a different matter and of some debate since he's not likely to explain it either.</li>
+                    <li className="list-group-item"><strong>Official Report:</strong><br/>To date Apok was key in rescuing an informant, and helped 
+                    establish the mercanary group we are just now beggining to hear about. It seems he still has greater stories ahead than behind,
+                    but we'll be watching closely to see how he chooses to advance.</li>
                 </ul>
             </div>
             </div>
