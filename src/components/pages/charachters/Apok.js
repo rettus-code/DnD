d6205dd1d2ddfae6c9223b5ccdbefbd7d14a5053
Rettus code <michael.rettusii@gmail.com>
2020-09-16 13:25:47 -0700

    more styling

--- src/components/pages/charachters/Apok.js
+++ src/components/pages/charachters/Apok.js
@@ -3,8 +3,21 @@ import '../../../App.css';
 
 export default function Apok() {
     return (
-        <div className="Apok">
-            
+        <div className="row">
+            <div className="col col-lg-4">
+            <div className="Apok"></div>
+            </div>
+            <div className="col col-lg-1"></div>
+            <div className="col col-lg-5">
+            <div className="card">
+                <div className="card-header">Apok</div>
+                <ul class="list-group list-group-flush">
+                    <li className="list-group-item">stuff</li>
+                    <li className="list-group-item">more stuff</li>
+                    <li className="list-group-item">final stuff</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }
