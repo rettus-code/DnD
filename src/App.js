6c397c80b04b97c3a912fb140d7ef380127d0eaf
Rettus code <michael.rettusii@gmail.com>
2020-09-15 18:44:12 -0700

    working in responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header';
 import Home from "./components/pages/Home";
+import Sharn from "./components/pages/Sharn";
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
     <div>
       <Header />
       <Route exact path="/" component={Home} />
-      {/* <Route exact path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
+      <Route exact path="/sharn" component={Sharn} />
+      {/* <Route path="/contact" component={Contact} />
       <Footer /> */}
     </div>
   </Router>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,9 +7,11 @@ export default function Home() {
         <div  className="home">
             <div className="jumbotron">
                 <h1 class="display-4">Welcome to Eberron!</h1>
-                    <p className="lead">This is a gaming world that will challenge you like no other. New content and exciting concepts to challenge even the most experienced characters to think on their feet. and find new ways to overcome challenges. </p>
-                    <hr className="my-4" />
-                    <p className="Oswald">Strange machines powered with magic at every turn the war torn planet of Eberron is recovering from it's past, is it destined to continue it's destruction or will a new breed of heroes bring new life and a new hope to it's citizens. Perhaps but that is not this story as we follow a group destined to be known, but not loved. They too wait for the day to be inspired to something greater, but that is not today.</p>
+                    <p className="lead">This is a gaming world that will challenge you like no other. New content and exciting concepts to push even the most experienced characters to think on their feet. and find new ways to overcome challenges. </p>
+                    
+            </div>
+            <div className="jumbotron2">
+
             </div>
         </div>
     )
