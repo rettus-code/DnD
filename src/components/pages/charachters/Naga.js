b14d6438dfaf76736d47ec13dc2f489a358c8d38
Rettus code <michael.rettusii@gmail.com>
2020-09-17 01:21:17 -0700

    er

--- src/components/pages/charachters/Naga.js
+++ src/components/pages/charachters/Naga.js
@@ -16,7 +16,7 @@ export default function Naga() {
                 has brought me here on a whim. Besides these guys bring a lot of opportunity to practice my skills without consequences. This is going to be so 
                 much fun making coin and blasting any opposition with my power. </li>
                 <li className="list-group-item"><strong>Official Report:</strong><br/>Unusual to see a drow on the surface but so far this group is everything
-                that could be unusual. Sorcers can be quite troublesome though this one is still young enough to be dealt with easily if alone. Timing will be 
+                that could be unusual. Sorcerers can be quite troublesome though this one is still young enough to be dealt with easily if alone. Timing will be 
                 important but only if deemed necesarry, hopefully this will become apparent soon before his strength grows.</li>
             </ul>
         </div>
