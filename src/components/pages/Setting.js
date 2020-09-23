50ac6f6e36d0c096678fa610628223cb82259f2a
Rettus code <michael.rettusii@gmail.com>
2020-09-23 05:20:19 -0700

    settings again

--- src/components/pages/Setting.js
+++ src/components/pages/Setting.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Setting() {
     return (
         <div>
+            <h1>Wondering what's going to go here?</h1>
             <p>Waiting for inputs from DM</p>
         </div>
     )
